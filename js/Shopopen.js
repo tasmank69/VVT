f2375c87d01af2ac49f0c1df78188bc5cdadd882
tasmank69 <54689622+tasmank69@users.noreply.github.com>
2021-05-19 10:21:52 +1200

    cool

--- js/Shopopen.js
+++ js/Shopopen.js
@@ -1,20 +1,29 @@
-var d = new Date();
-var n = d.getDay()
-if (n >= 3) {
-    var h = d.getHours
-    var m = d.getMinutes
-    if (h = 9 && m >= 30)   {
-        var open = 1
-        
-    } else if (h >= 10 && h<=2) {
-        var open = 1
-    } else {var open = 0}
-}
-if (open = 1) {
-    document.getElementById("openclose").innerHTML = "Open";
-    document.getElementById("openclose").style.color = "green";
-}
-if (open = 1) {
-    document.getElementById("openclose").innerHTML = "Closed";
-    document.getElementById("openclose").style.color = "red";
-}
+function ShopOpen() {
+    var d = new Date();
+    console.log(d)
+    var n = d.getDay();
+    console.log(n)
+    if (n >= 3) {
+        var h = d.getHours;
+        var m = d.getMinutes;
+        if (h == 9 && m >= 30)   {
+            var open = true 
+            
+        } else if (h >= 10 && h <= 2) {
+            var open = true 
+        } else {var open = false};
+    } else {
+        var open = false
+    };
+    console.log(open)
+    if (open == true) {
+        document.getElementById("preopen").innerHTML = "Yes, We are";
+        document.getElementById("openclose").innerHTML = "Open";
+        document.getElementById("openclose").style.color = "green";
+        document.getElementById("sign").style.borderColor = "green";
+    } else {
+        document.getElementById("preopen").innerHTML = "Sorry, We are";
+        document.getElementById("openclose").innerHTML = "Closed";
+        document.getElementById("openclose").style.color = "red";
+        document.getElementById("sign").style.borderColor = "red";
+    };};

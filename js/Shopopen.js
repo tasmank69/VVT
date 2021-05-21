6a8689004970a2a4437c27b157aba726a35454b6
tasmank69 <54689622+tasmank69@users.noreply.github.com>
2021-05-21 13:17:22 +1200

    sazdxfcnjmkl

--- js/Shopopen.js
+++ js/Shopopen.js
@@ -1,9 +1,11 @@
 function ShopOpen() {
+    var open = undefined
     var d = new Date();
     console.log(d)
     var n = d.getDay();
     console.log(n)
     if (n >= 3) {
+        console.log("stage 1")
         var h = d.getHours;
         var m = d.getMinutes;
         if (h == 9 && m >= 30)   {
@@ -15,15 +17,17 @@ function ShopOpen() {
     } else {
         var open = false
     };
-    console.log(open)
+    console.log(open);
     if (open == true) {
         document.getElementById("preopen").innerHTML = "Yes, We are";
         document.getElementById("openclose").innerHTML = "Open";
         document.getElementById("openclose").style.color = "green";
         document.getElementById("sign").style.borderColor = "green";
-    } else {
+        console.log("open");
+    } else if (open == false) {
         document.getElementById("preopen").innerHTML = "Sorry, We are";
         document.getElementById("openclose").innerHTML = "Closed";
         document.getElementById("openclose").style.color = "red";
         document.getElementById("sign").style.borderColor = "red";
+        console.log("closed");
     };};
